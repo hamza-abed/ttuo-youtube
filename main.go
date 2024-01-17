452126ef683d00edd1ff7062c29b22270d9cac5e
habed <hamza.abed.fr@gmail.com>
2024-01-17 10:14:13 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", pingHandler)
+	port := 8080
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "application/json")
+	response := map[string]string{"messagclear
+
+	json.NewEncoder(w).Encode(response)
+}
